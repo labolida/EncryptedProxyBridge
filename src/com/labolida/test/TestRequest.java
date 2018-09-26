d443e1d249abf2009db6d7638c3d1dc85d0ac7b9
Leonardo LNML. Martin Labolida <leonardo.martin@stkproj.com>
2018-09-26 14:40:49 +0200

    FIX BUG test reading from socket [SOLVED]

--- src/com/labolida/test/TestRequest.java
+++ src/com/labolida/test/TestRequest.java
@@ -1,5 +1,7 @@
 package com.labolida.test;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -12,23 +14,24 @@ public class TestRequest {
 			OutputStream out = socket.getOutputStream();
 			InputStream in = socket.getInputStream();
 			
-			Thread.sleep(200);
+			Thread.sleep(20);
 			
 			out.write("GET /login.php HTTP/1.1\n\n".getBytes());
 			out.write("\n\n".getBytes());
 			
-			byte buff[] = new byte[1024*8];
-			int length = in.read(buff);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			String something;
 
-			in.read( buff );
-			
-			System.out.println("TestRequest: received:" + new String(buff) );
-			
+			  while ((something = reader.readLine()) != null){
+			    	 System.out.println("" + something );
+			  }
 		}
 		catch (Exception e) {
 			System.out.println("error at " + TestRequest.class.getName()+ " message:" + e.getMessage());
 		}
-
 	}
-
 }
+/*
+PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+*/
---
+++ src/runNode2.bat
@@ -0,0 +1,1 @@
+java com.labolida.startup.Startup 964 127.0.0.1 8000
